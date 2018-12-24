168ea480b8f001f3c2495cf8d7bd7b7d44a1b96a
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-12-24 14:48:33 -0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: 'server.js',
+    script: 'rest-blog/server.js',
     key: 'my-website.pem',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
