144cd782bea95d4e9c8185b410f369bd1be739d1
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2018-12-25 19:37:39 -0800

    trying to set up multer image upload to s3

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -3,35 +3,52 @@ const express = require("express");
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth')
 const multer = require('multer')
+const multers3 = require('multer-s3')
+const aws = require('aws-sdk')
 
 const PostsController = require('../controllers/posts')
 
 
 // ## THIS CONTROLS THE IMAGE UPLOAD ## //
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, './uploads/postImages')
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname)
-        // cb(null, new Date().toISOString() + file.originalname)
-    }
+
+aws.config.update({
+    secretAccessKey: "RCtfDEfNb2zgkLBb3PtbsjcwgPsfXk8+a8z3ckyt",
+    accessKeyId: "AKIAJNCQ2A2K4J76JO6Q",
+    region: "us-west-2"
 })
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true)
-    } else {
-        cb(new Error('File type not accepted.'), false)
-    }
-}
+const s3 = new aws.S3()
+
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, './uploads/postImages')
+//     },
+//     filename: function(req, file, cb) {
+//         cb(null, file.originalname)
+//         // cb(null, new Date().toISOString() + file.originalname)
+//     }
+// })
+
+// const fileFilter = (req, file, cb) => {
+//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//         cb(null, true)
+//     } else {
+//         cb(new Error('File type not accepted.'), false)
+//     }
+// }
 
 const upload = multer({
-    storage: storage,
-    limits: {
-        filesize: 1024 * 1024 * 5,
-    },
-    fileFilter: fileFilter
+    storage: multers3({
+        s3: s3,
+        bucket: 'tylorkolbeck.com',
+        acl: 'public-read',
+        metadata: function(req, file, cb) {
+            cb(null, {fieldName: file.fieldName})
+        },
+        key: function(req, file, cb) {
+            cb(null, Date.now().toString())
+        }
+    })
 })
 // ###### END MULTER SETUP ###### //
 
@@ -49,7 +66,7 @@ router.get("/filter/", PostsController.posts_filter_tag);
 // posts/
 // Adds a post to posts
 // router.post("/", upload.array('postImages', 10), PostsController.posts_create_post);
-router.post("/", checkAuth, upload.array('postImages', 10), PostsController.posts_create_post);
+router.post("/", checkAuth, upload.single('postImages'), PostsController.posts_create_post);
 
 // GET
 // posts/:postid
