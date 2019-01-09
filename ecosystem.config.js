271a99e36f724b4e3c36699470c78dd94b757be6
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-01-08 17:01:44 -0800

    pm2 testing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,33 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'server.js',
-    key: 'my-website.pem',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production',
+    apps : [{
+      name: 'API',
+      script: 'server.js',
+      key: '~/.ssh/id_rsa.pub',
+  
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      }
+    }],
+  
+    deploy : {
+      production : {
+        key  : '~/.ssh/id_rsa.pub',
+        user : 'ubuntu',
+        host : '142.93.95.14',
+        ref  : 'origin/master',
+        repo : 'https://github.com/tylorkolbeck/rest-blog.git',
+        path : '/dev',
+        'post-deploy' : 'npm install && pm2 reload server'
+      }
     }
-  }],
-
-  deploy : {
-    production : {
-      key  : 'my-website.pem',
-      user : 'ubuntu',
-      host : 'ec2-54-148-26-236.us-west-2.compute.amazonaws.com',
-      ref  : 'origin/master',
-      repo : 'https://github.com/tylorkolbeck/rest-blog.git',
-      path : '/home/bitnami/apps/rest-blog',
-      'post-deploy' : 'npm install && sudo pm2 reload server'
-    }
-  }
-};
+  };
+  
---
+++ ecosystem.config2.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: 'server.js',
+    key: 'my-website.pem',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    }
+  }],
+
+  deploy : {
+    production : {
+      key  : 'my-website.pem',
+      user : 'ubuntu',
+      host : 'ec2-54-148-26-236.us-west-2.compute.amazonaws.com',
+      ref  : 'origin/master',
+      repo : 'https://github.com/tylorkolbeck/rest-blog.git',
+      path : '/home/bitnami/apps/rest-blog',
+      'post-deploy' : 'npm install && sudo pm2 reload server'
+    }
+  }
+};
