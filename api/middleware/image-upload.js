8b081921a48385d3a2b4adda21a1e89865156726
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2019-01-31 20:50:47 -0800

    adding image to seperate into there own  directories

--- api/middleware/image-upload.js
+++ api/middleware/image-upload.js
@@ -22,7 +22,7 @@ const upload = multer({
         },
         key: function(req, file, cb) {
             // let newFileName = Date.now().toString() + "-" + file.originalname
-            let fullPath = 'imageUploads/' + file.originalname
+            let fullPath = 'imageUploads/' + directoryName + '/' + file.originalname
             cb(null, fullPath)
         }
     })
